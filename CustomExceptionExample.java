b639a15b61cd87a0bdd227dd6446c469a342f363
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-01-17 22:38:18 +0530

    Create CustomExceptionExample.java

---
+++ CustomExceptionExample.java
@@ -0,0 +1,23 @@
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExceptionExample {
+    public static void main(String[] args) {
+        try {
+            checkAge(15);
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught an exception: " + e.getMessage());
+        }
+    }
+
+    public static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or older to register.");
+        } else {
+            System.out.println("Age is valid. You are registered!");
+        }
+    }
+}
