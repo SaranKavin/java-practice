97b9ae21f5f0b53d96142a744050665d4f47c0c9
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-02-24 21:07:14 +0530

    Create LargestElementInArray.java

---
+++ LargestElementInArray.java
@@ -0,0 +1,23 @@
+
+class LargestElementInArray {
+  
+    static int arr[] = {20, 10, 20, 4, 100}; 
+    
+    static int largest() {
+      
+        int i; 
+        
+        int max = arr[0]; 
+        
+        for (i = 1; i < arr.length; i++) 
+            if (arr[i] > max) 
+                max = arr[i]; 
+        
+        return max; 
+    } 
+    
+    public static void main(String[] args) {
+      
+        System.out.println(largest()); 
+    } 
+} 
