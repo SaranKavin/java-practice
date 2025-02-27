5c13d3da863bf90d7c704edf3761cc3d2872592f
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-02-27 19:29:06 +0530

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,22 @@
+class Person {
+  
+    void role() {
+        System.out.println("I am a person.");
+    }
+}
+
+class Father extends Person {
+  
+    @Override
+    void role() {
+        System.out.println("I am a father.");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+      
+        Person p = new Father();
+        p.role();  
+    }
+}
