5bca0e3c6710932546aa0b20687cd075bcf3838a
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-03-06 22:21:43 +0530

    Create Abstraction.java

---
+++ Abstraction.java
@@ -0,0 +1,24 @@
+abstract class Abstraction {
+    abstract void turnOn();
+    abstract void turnOff();
+}
+
+class TVRemote extends Geeks {
+    @Override
+    void turnOn() {
+        System.out.println("TV is turned ON.");
+    }
+
+    @Override
+    void turnOff() {
+        System.out.println("TV is turned OFF.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Geeks remote = new TVRemote();
+        remote.turnOn();   
+        remote.turnOff();  
+    }
+}
