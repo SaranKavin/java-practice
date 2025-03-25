127565d9115ec6327854e1345d9e7de3530627dd
SARAN P <138579247+SaranKavin@users.noreply.github.com>
2025-03-25 21:46:42 +0530

    Create VectorExample.java

---
+++ VectorExample.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class VectorExample 
+{
+    public static void main(String[] args) 
+    {
+      
+        Vector<Integer> v = new Vector<>(3, 2);
+        v.addElement(1);
+        v.addElement(2);
+        v.addElement(3);
+        v.insertElementAt(0, 1);
+        v.removeElementAt(2);
+        for (int i : v) {
+            System.out.println(i);
+        }
+    }
+}
